5ed49879bbf3bf88b12f498df95e8b2aea8549e2
mole828 <mrx800@163.com>
2024-11-14 08:13:32 +0800

    Initial commit

---
+++ src/main/kotlin/top/moles/Application.kt
@@ -0,0 +1,13 @@
+package top.moles
+
+import io.ktor.server.application.*
+import top.moles.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
