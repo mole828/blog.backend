6ad6949e044e05d819419ecf5a093d9d9389bb63
mole828 <mrx800@163.com>
2024-11-14 14:17:09 +0800

    feat: "Hello, Ktor." page

--- src/main/kotlin/top/moles/plugins/Routing.kt
+++ src/main/kotlin/top/moles/plugins/Routing.kt
@@ -1,13 +1,26 @@
 package top.moles.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.html.*
 import io.ktor.server.routing.*
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.title
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val hello = "Hello, Ktor."
+            call.respondHtml {
+                head {
+                    title = hello
+                }
+                body {
+                    h1 {
+                        +hello
+                    }
+                }
+            }
         }
     }
 }
